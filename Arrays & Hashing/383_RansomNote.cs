3bf6b64c6180c74ac4a6864a4fb9733127e1a2f0
Your Name <andrzejhk@gmail.com>
2026-06-03 18:21:29 +0200

    optimized #383

--- Arrays & Hashing/383_RansomNote.cs
+++ Arrays & Hashing/383_RansomNote.cs
@@ -6,6 +6,28 @@ public static class RansomNote
     //  ransomNote = "aa"
     public static bool CanConstruct(string ransomNote, string magazine)
     {
+        int[] counts = new int[26];
+
+        foreach (char c in magazine)
+        {
+            counts[c - 'a']++;
+        }
+
+        foreach (char c in ransomNote)
+        {
+            if (counts[c - 'a'] <= 0)
+            {
+                return false;
+            }
+
+            counts[c - 'a'] -= 1;
+        }
+
+        return true;
+    }
+
+    public static bool CanConstruct2(string ransomNote, string magazine)
+    {
         Dictionary<char, int> dict = [];
 
         foreach (char c in magazine)
