ee98b5ac31f3a5644bfda5a6d9a6e200acf890ce
Your Name <andrzejhk@gmail.com>
2026-06-08 16:27:08 +0200

    optimized

--- Arrays & Hashing/128_LongestConsecutiveSequence.cs
+++ Arrays & Hashing/128_LongestConsecutiveSequence.cs
@@ -1,8 +1,30 @@
 public static class LongestConsecutive
 {
+    //22 ms, beats 68.10%
+    public static int FindLongestConsecutive(int[] nums)
+    {
+        HashSet<int> set = new HashSet<int>([.. nums]);
+        int longest = 0;
+        foreach (var num in set)
+        {
+            if (!set.Contains(num - 1))
+            {
+                int currentNum = num;
+                int currentStreak = 1;
+                while (set.Contains(currentNum + 1))
+                {
+                    currentStreak++;
+                    currentNum++;
+                }
+                longest = Math.Max(currentStreak, longest);
+            }
+        }
+        return longest;
+    }
+
     // 70 ms,beats 41.52%
     // memory 66.99, beats 95.55%
-    public static int FindLongestConsecutive(int[] nums)
+    public static int FindLongestConsecutive2(int[] nums)
     {
         if (nums.Length == 0)
         {
