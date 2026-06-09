9823e1988f180455c4698610e82cf560034639c4
dreikovich <andrzejhk@gmail.com>
2026-06-09 16:30:28 +0200

    #344

---
+++ Two Pointers & Sliding Window/344_ReverseString.cs
@@ -0,0 +1,25 @@
+namespace algorithms.Two_Pointers___Sliding_Window;
+
+public class Str {
+    // 0 ms, beats 100%
+    public void ReverseString(char[] s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right)
+        {
+            //Tuple Swap
+            (s[left], s[right]) = (s[right], s[left]);
+            left++;
+            right--;
+        }
+    }
+}
+
+//Tuple Swap
+//(s[left], s[right]) = (s[right], s[left]);
+// it is the same as code below (with buffer/temp)
+// char buffer = s[left];
+// s[left] = s[right];
+// s[right] = buffer;
