107ff277c6d11fda1486b45c78617f72e90967f5
dreikovich <andrzejhk@gmail.com>
2026-06-19 15:46:05 +0200

    #876

---
+++ Two Pointers & Sliding Window/876_MiddleOfTheLinkedList.cs
@@ -0,0 +1,28 @@
+public class Solution
+{
+    // 0 ms, beats 100%
+    public ListNode MiddleNode(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
